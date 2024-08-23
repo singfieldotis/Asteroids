c713aeeac5d909516cfc6d427ec631c25ee431a8
Otis Singfield <singfieldotis@gmail.com>
2024-08-23 00:04:21 -0400

    testing file import

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from constants import *
 
 def main():
     print("Starting asteroids!")
-    print(SCREEN_WIDTH)
-    print(SCREEN_HEIGHT)
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()

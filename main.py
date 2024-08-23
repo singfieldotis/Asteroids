2065801c60079951888fee427cb3db1ca934772a
Otis Singfield <singfieldotis@gmail.com>
2024-08-22 23:55:00 -0400

    Setting up file tree

---
+++ main.py
@@ -0,0 +1,14 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+
+def main():
+    print("Starting asteroids!")
+    print(SCREEN_WIDTH)
+    print(SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+    main()

6a8e48964742de88e386f6ce9757310ba9293ef8
Otis Singfield <singfieldotis@gmail.com>
2024-08-29 23:11:52 -0400

    create working game window

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ from constants import *
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game = True
+    while game == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()

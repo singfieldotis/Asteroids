c42a3075e361a59a8993501e83c859147c930963
Otis Singfield <singfieldotis@gmail.com>
2025-05-12 13:33:51 -0400

    get ship on screen

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        dt += clock.get_time() / 1000
+
 
 if __name__ == "__main__":
     main()

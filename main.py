a39c4db3106c9f3e6441b33ae913c2e06cadadf3
Otis Singfield <singfieldotis@gmail.com>
2025-05-12 13:46:49 -0400

    make ship rotate left and right

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ from player import Player
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
-        dt += clock.get_time() / 1000
+
+        # limit the framerate to 60 fps
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
